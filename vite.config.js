8928a2d9b8db68833b67071e1b528621859f8a26
ssiissymhnn <ssiissy.mhnn@gmail.com>
2026-05-13 16:07:55 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: '127.0.0.1',
+    port: 4173,
+    strictPort: true,
+  },
 })
